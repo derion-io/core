efdbbab704f3d24f1ccf14ea657e0211671ca69d
Anh Ngo <anhnq82@gmail.com>
2023-03-21 16:01:06 +0700

    Init env

---
+++ README.md
@@ -0,0 +1,1 @@
+# Derivable Core Contracts
---
+++ hardhat.config.js
@@ -0,0 +1,96 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+const dotenv = require("dotenv");
+dotenv.config({ path: __dirname + "/.env" });
+
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-contract-sizer");
+require("@openzeppelin/hardhat-upgrades");
+require("hardhat-gas-reporter");
+
+module.exports = {
+    defaultNetwork: 'hardhat',
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.13",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 1000000,
+                    },
+                },
+            },
+            {
+                version: "0.6.6",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 1000000,
+                    },
+                },
+            }
+        ]
+    },
+    networks: {
+        hardhat: {
+            accounts: [
+                {
+                    privateKey: "0x0000000000000000000000000000000000000000000000000000000000000001",
+                    balance: "900000000000000000000000000000000000",
+                },
+                {
+                    privateKey: '0x0000000000000000000000000000000000000000000000000000000000000002',
+                    balance: "900000000000000000000000000000000000",
+                },
+                {
+                    privateKey: '0x0000000000000000000000000000000000000000000000000000000000000003',
+                    balance: "1000000000000000000000000",
+                },
+            ]
+        },
+        mainnet: {
+            url: process.env.BSC_MAINNET_PROVIDER ?? 'https://bsc-dataseed3.binance.org/',
+            accounts: [
+                process.env.MAINNET_DEPLOYER ?? '0x0000000000000000000000000000000000000000000000000000000000000001',
+            ],
+            timeout: 900000,
+            chainId: 56
+        },
+        testnet: {
+            url: process.env.BSC_TESTNET_PROVIDER ?? '',
+            accounts: [
+                process.env.TESTNET_DEPLOYER ?? '0x0000000000000000000000000000000000000000000000000000000000000001',
+            ],
+            timeout: 20000,
+            chainId: 97
+        },
+        ganache: {
+            url: 'http://127.0.0.1:8545',
+            // kick balcony people guess oppose verb faint explain spoil learn that pool
+            accounts: [
+                '60f5906de1edfc4d14eb4aea49ed4c06641bbdbd5a56092392308e9730598373',
+                '70ddda4400c15d2daa517f858defab22c8a5d9adeaf3d74caa5ca86a5959ddd9'
+            ],
+            timeout: 900000,
+            chainId: 1337
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545",
+            accounts: [
+                process.env.LOCAL_DEPLOYER ?? '0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908c',
+                '0x0000000000000000000000000000000000000000000000000000000000000001'
+            ]
+        }
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+    contractSizer: {
+        alphaSort: true,
+        disambiguatePaths: false,
+        runOnCompile: true,
+        strict: true,
+        only: [],
+    }
+};
