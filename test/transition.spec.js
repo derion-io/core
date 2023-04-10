eee37c1f07d687f7434eb57f551546881f46c0e6
BerlinP <bknghi95@gmail.com>
2023-04-10 10:32:24 +0700

    Transition UT amountIn lte amountInDesired

---
+++ test/transition.spec.js
@@ -0,0 +1,324 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { expect, use } = require("chai");
+const { solidity } = require("ethereum-waffle");
+const { weiToNumber, bn, getDeltaSupply, numberToWei, packId, unpackId, encodeSqrtX96 } = require("./shared/utilities");
+
+use(solidity)
+
+const opts = {
+  gasLimit: 30000000
+}
+
+const TRANSFER_FROM_SENDER = 0
+const TRANSFER_FROM_ROUTER = 1
+const TRANSFER_CALL_VALUE = 2
+const IN_TX_PAYMENT = 4
+const ALLOWANCE_BRIDGE = 8
+const AMOUNT_EXACT = 0
+const AMOUNT_ALL = 1
+const EIP_ETH = 0
+const ID_721_ALL = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("UniversalTokenRouter.ID_721_ALL"))
+const ACTION_IGNORE_ERROR = 1
+const ACTION_RECORD_CALL_RESULT = 2
+const ACTION_INJECT_CALL_RESULT = 4
+
+const HALF_LIFE = 10 * 365 * 24 * 60 * 60
+
+// const HALF_LIFE = 0
+
+describe("Decay funding rate", function () {
+  async function deployDDLv2() {
+    const [owner, accountA, accountB] = await ethers.getSigners();
+    const signer = owner;
+    // deploy token1155
+
+    const UTR = require("@derivable/utr/build/UniversalTokenRouter.json")
+    const UniversalRouter = new ethers.ContractFactory(UTR.abi, UTR.bytecode, owner)
+    const utr = await UniversalRouter.deploy()
+    await utr.deployed()
+
+    // deploy pool factory
+    const PoolFactory = await ethers.getContractFactory("PoolFactory");
+    const poolFactory = await PoolFactory.deploy(
+      // derivable1155.address
+    );
+    // weth test
+    const compiledWETH = require("canonical-weth/build/contracts/WETH9.json")
+    const WETH = await new ethers.ContractFactory(compiledWETH.abi, compiledWETH.bytecode, signer);
+    // uniswap factory
+    const compiledUniswapFactory = require("./compiled/UniswapV3Factory.json");
+    const UniswapFactory = await new ethers.ContractFactory(compiledUniswapFactory.abi, compiledUniswapFactory.bytecode, signer);
+    // uniswap router
+
+    const compiledUniswapv3Router = require("./compiled/SwapRouter.json");
+    const UniswapRouter = new ethers.ContractFactory(compiledUniswapv3Router.abi, compiledUniswapv3Router.bytecode, signer);
+    // uniswap PM
+
+    const compiledUniswapv3PositionManager = require("./compiled/NonfungiblePositionManager.json");
+    const UniswapPositionManager = new ethers.ContractFactory(compiledUniswapv3PositionManager.abi, compiledUniswapv3PositionManager.bytecode, signer);
+    // erc20 factory
+
+    const compiledERC20 = require("@uniswap/v2-core/build/ERC20.json");
+    const erc20Factory = new ethers.ContractFactory(compiledERC20.abi, compiledERC20.bytecode, signer);
+    // setup uniswap
+
+    const usdc = await erc20Factory.deploy(numberToWei(100000000000));
+    const weth = await WETH.deploy();
+    const uniswapFactory = await UniswapFactory.deploy();
+    const uniswapRouter = await UniswapRouter.deploy(uniswapFactory.address, weth.address);
+    const uniswapPositionManager = await UniswapPositionManager.deploy(uniswapFactory.address, weth.address, '0x0000000000000000000000000000000000000000')
+
+    await uniswapFactory.createPool(usdc.address, weth.address, 500)
+
+    const compiledUniswapPool = require("./compiled/UniswapV3Pool.json");
+    const pairAddress = await uniswapFactory.getPool(usdc.address, weth.address, 500)
+    const uniswapPair = new ethers.Contract(pairAddress, compiledUniswapPool.abi, signer);
+
+    await usdc.approve(uniswapRouter.address, ethers.constants.MaxUint256);
+    await weth.approve(uniswapRouter.address, ethers.constants.MaxUint256);
+
+    const quoteTokenIndex = weth.address.toLowerCase() < usdc.address.toLowerCase() ? 1 : 0
+    const initPriceX96 = encodeSqrtX96(quoteTokenIndex ? 1500 : 1, quoteTokenIndex ? 1 : 1500)
+    const a = await uniswapPair.initialize(initPriceX96)
+    a.wait(1);
+
+    await time.increase(1000);
+
+    const AsymptoticPerpetual = await ethers.getContractFactory("AsymptoticPerpetual");
+
+    const asymptoticPerpetual = await AsymptoticPerpetual.deploy();
+    await asymptoticPerpetual.deployed();
+
+    // deploy token1155
+    const Token = await ethers.getContractFactory("Token")
+    const derivable1155 = await Token.deploy(
+      "Test/",
+      utr.address
+    )
+    await derivable1155.deployed()
+
+    // deploy ddl pool
+    const oracle = ethers.utils.hexZeroPad(
+      bn(quoteTokenIndex).shl(255).add(bn(300).shl(256 - 64)).add(uniswapPair.address).toHexString(),
+      32,
+    )
+    const params = {
+      utr: utr.address,
+      token: derivable1155.address,
+      logic: asymptoticPerpetual.address,
+      oracle,
+      reserveToken: weth.address,
+      recipient: owner.address,
+      mark: bn(25).shl(112).div(1000),
+      k: 5,
+      a: '30000000000',
+      b: '30000000000',
+      halfLife: HALF_LIFE // ten years
+    }
+    const poolAddress = await poolFactory.computePoolAddress(params);
+    let txSignerA = weth.connect(accountA);
+    let txSignerB = weth.connect(accountB);
+
+    await txSignerA.deposit({
+      value: '100000000000000000000000000000'
+    })
+    await txSignerB.deposit({
+      value: '100000000000000000000000000000'
+    })
+    await weth.deposit({
+      value: '100000000000000000000000000000'
+    })
+    await weth.transfer(poolAddress, numberToWei(1));
+    await poolFactory.createPool(params);
+    const derivablePool = await ethers.getContractAt("Pool", await poolFactory.computePoolAddress(params));
+
+    await time.increase(100);
+    // deploy helper
+    const DerivableHelper = await ethers.getContractFactory("Helper")
+    const derivableHelper = await DerivableHelper.deploy(
+      derivablePool.address,
+      derivable1155.address
+    )
+    await derivableHelper.deployed()
+    const A_ID = packId(0x10, derivablePool.address);
+    const B_ID = packId(0x20, derivablePool.address);
+    const R_ID = packId(0x00, derivablePool.address);
+    const C_ID = packId(0x30, derivablePool.address);
+    await weth.approve(derivablePool.address, '100000000000000000000000000');
+
+    txSignerA = weth.connect(accountA);
+    txSignerB = weth.connect(accountB);
+    await txSignerA.approve(derivablePool.address, '100000000000000000000000000');
+    await txSignerB.approve(derivablePool.address, '100000000000000000000000000');
+    txSignerA = derivable1155.connect(accountA);
+    await txSignerA.setApprovalForAll(derivablePool.address, true);
+    txSignerB = derivable1155.connect(accountB);
+    await txSignerB.setApprovalForAll(derivablePool.address, true);
+    txSignerA = derivablePool.connect(accountA);
+    txSignerB = derivablePool.connect(accountB);
+
+    await txSignerA.exactIn(
+      0x00,
+      numberToWei(0.5),
+      0x30,
+      '0x0000000000000000000000000000000000000000',
+      accountA.address
+    );
+
+    return {
+      C_ID,
+      A_ID,
+      B_ID,
+      utr,
+      owner,
+      weth,
+      derivablePool,
+      derivable1155,
+      derivableHelper,
+      accountA,
+      accountB,
+      txSignerA,
+      txSignerB
+    }
+  }
+
+  describe("Pool", function () {
+    async function amountInMustGteAmountInDesired(longAmount, rateLongSwapback, shortAmount, rateShortSwapback, period, prefix) {
+      const { accountB, accountA, txSignerA, txSignerB, weth, derivable1155, A_ID, B_ID } = await loadFixture(deployDDLv2);
+      await txSignerA.exactIn(
+        0x00,
+        numberToWei(1),
+        0x30,
+        '0x0000000000000000000000000000000000000000',
+        accountA.address
+      );
+      const wethABegin = await weth.balanceOf(accountA.address)
+      const wethBBegin = await weth.balanceOf(accountB.address)
+      await txSignerA.exactIn(
+        0x00,
+        longAmount,
+        0x10,
+        '0x0000000000000000000000000000000000000000',
+        accountA.address
+      );
+      await txSignerB.exactIn(
+        0x00,
+        shortAmount,
+        0x20,
+        '0x0000000000000000000000000000000000000000',
+        accountB.address
+      );
+      const wethAAfter = await weth.balanceOf(accountA.address)
+      const wethBAfter = await weth.balanceOf(accountB.address)
+      if (period > 0) {
+        await time.increase(period)
+      }
+      expect(wethABegin.sub(wethAAfter)).to.be.lte(longAmount, `${prefix}: Long R->A In > Desired`)
+      expect(wethBBegin.sub(wethBAfter)).to.be.lte(shortAmount, `${prefix}: Long R->B In > Desired`)
+
+      const tokenAAmountBefore = await derivable1155.balanceOf(accountA.address, A_ID)
+      const tokenBAmountBefore = await derivable1155.balanceOf(accountB.address, B_ID)
+      
+      const amountAIn = tokenAAmountBefore.mul(rateLongSwapback).div(100)
+      const amountBIn = tokenBAmountBefore.mul(rateShortSwapback).div(100)
+      await txSignerA.exactIn(
+        0x10,
+        amountAIn,
+        0x00,
+        '0x0000000000000000000000000000000000000000',
+        accountA.address
+      );
+      await txSignerB.exactIn(
+        0x20,
+        amountBIn,
+        0x00,
+        '0x0000000000000000000000000000000000000000',
+        accountB.address
+      );
+      const tokenAAmountAfter = await derivable1155.balanceOf(accountA.address, A_ID)
+      const tokenBAmountAfter = await derivable1155.balanceOf(accountB.address, B_ID)
+      
+      expect(tokenAAmountBefore.sub(tokenAAmountAfter)).to.be.gte(amountAIn, `${prefix}: Long A->R In > Desired`)
+      expect(tokenBAmountBefore.sub(tokenBAmountAfter)).to.be.gte(amountBIn, `${prefix}: Long B->R In > Desired`)
+    }
+    describe("In > Desired", function () {
+      it("1e Long - 100% back, 1e Short - 100% back", async function () {
+        await amountInMustGteAmountInDesired(
+          numberToWei(1),
+          100,
+          numberToWei(1),
+          100,
+          "1e Long - 50% back, 1e Short - 50% back"
+        )
+      })
+      it("1e Long - 50% back, 0.5e Short - 50% back", async function () {
+        await amountInMustGteAmountInDesired(
+          numberToWei(1),
+          50,
+          numberToWei(0.5),
+          50,
+          "1e Long - 50% back, 0.5e Short - 50% back"
+        )
+      })
+      it("1e Long - 30% back, 1e Short - 40% back", async function () {
+        await amountInMustGteAmountInDesired(
+          numberToWei(1),
+          30,
+          numberToWei(1),
+          40,
+          "1e Long - 30% back, 1e Short - 40% back"
+        )
+      })
+      it("0.7e Long - 33% back, 0.9e Short - 49% back", async function () {
+        await amountInMustGteAmountInDesired(
+          numberToWei(0.7),
+          33,
+          numberToWei(0.9),
+          49,
+          "0.7e Long - 33% back, 0.9e Short - 49% back"
+        )
+      })
+      it("1e Long - 90% back, 0.1e Short - 100% back", async function () {
+        await amountInMustGteAmountInDesired(
+          numberToWei(1),
+          90,
+          numberToWei(0.1),
+          100,
+          "1e Long - 90% back, 0.1e Short - 100% back"
+        )
+      })
+      it("0.1e Long - 70% back, 1e Short - 30% back", async function () {
+        await amountInMustGteAmountInDesired(
+          numberToWei(0.1),
+          70,
+          numberToWei(1),
+          30,
+          "0.1e Long - 70% back, 1e Short - 30% back"
+        )
+      })
+      it("0.7e Long - 33% back, 0.9e Short - 49% back, wait HL", async function () {
+        await amountInMustGteAmountInDesired(
+          numberToWei(0.7),
+          33,
+          numberToWei(0.9),
+          49,
+          HALF_LIFE,
+          "0.7e Long - 33% back, 0.9e Short - 49% back"
+        )
+      })
+      it("1e Long - 90% back, 0.1e Short - 100% back, wait 0.5 HL", async function () {
+        await amountInMustGteAmountInDesired(
+          numberToWei(1),
+          90,
+          numberToWei(0.1),
+          100,
+          0.5 * HALF_LIFE,
+          "1e Long - 90% back, 0.1e Short - 100% back"
+        )
+      })
+    })
+  });
+})
