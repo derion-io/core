81af7e978eeb31a254de5c6a24a4edf2bc4b5b15
BerlinP <bknghi95@gmail.com>
2023-04-11 10:53:13 +0700

    Update UT

--- test/decay.spec.js
+++ test/decay.spec.js
@@ -34,12 +34,12 @@ HLs.forEach(HALF_LIFE => {
       const [owner, accountA, accountB] = await ethers.getSigners();
       const signer = owner;
       // deploy token1155
-  
+
       const UTR = require("@derivable/utr/build/UniversalTokenRouter.json")
       const UniversalRouter = new ethers.ContractFactory(UTR.abi, UTR.bytecode, owner)
       const utr = await UniversalRouter.deploy()
       await utr.deployed()
-  
+
       // deploy pool factory
       const PoolFactory = await ethers.getContractFactory("PoolFactory");
       const poolFactory = await PoolFactory.deploy(
@@ -52,46 +52,46 @@ HLs.forEach(HALF_LIFE => {
       const compiledUniswapFactory = require("./compiled/UniswapV3Factory.json");
       const UniswapFactory = await new ethers.ContractFactory(compiledUniswapFactory.abi, compiledUniswapFactory.bytecode, signer);
       // uniswap router
-  
+
       const compiledUniswapv3Router = require("./compiled/SwapRouter.json");
       const UniswapRouter = new ethers.ContractFactory(compiledUniswapv3Router.abi, compiledUniswapv3Router.bytecode, signer);
       // uniswap PM
-  
+
       const compiledUniswapv3PositionManager = require("./compiled/NonfungiblePositionManager.json");
       const UniswapPositionManager = new ethers.ContractFactory(compiledUniswapv3PositionManager.abi, compiledUniswapv3PositionManager.bytecode, signer);
       // erc20 factory
-  
+
       const compiledERC20 = require("@uniswap/v2-core/build/ERC20.json");
       const erc20Factory = new ethers.ContractFactory(compiledERC20.abi, compiledERC20.bytecode, signer);
       // setup uniswap
-  
+
       const usdc = await erc20Factory.deploy(numberToWei(100000000000));
       const weth = await WETH.deploy();
       const uniswapFactory = await UniswapFactory.deploy();
       const uniswapRouter = await UniswapRouter.deploy(uniswapFactory.address, weth.address);
       const uniswapPositionManager = await UniswapPositionManager.deploy(uniswapFactory.address, weth.address, '0x0000000000000000000000000000000000000000')
-  
+
       await uniswapFactory.createPool(usdc.address, weth.address, 500)
-  
+
       const compiledUniswapPool = require("./compiled/UniswapV3Pool.json");
       const pairAddress = await uniswapFactory.getPool(usdc.address, weth.address, 500)
       const uniswapPair = new ethers.Contract(pairAddress, compiledUniswapPool.abi, signer);
-  
+
       await usdc.approve(uniswapRouter.address, ethers.constants.MaxUint256);
       await weth.approve(uniswapRouter.address, ethers.constants.MaxUint256);
-  
+
       const quoteTokenIndex = weth.address.toLowerCase() < usdc.address.toLowerCase() ? 1 : 0
       const initPriceX96 = encodeSqrtX96(quoteTokenIndex ? 1500 : 1, quoteTokenIndex ? 1 : 1500)
       const a = await uniswapPair.initialize(initPriceX96)
       a.wait(1);
-  
+
       await time.increase(1000);
-  
+
       const AsymptoticPerpetual = await ethers.getContractFactory("AsymptoticPerpetual");
-  
+
       const asymptoticPerpetual = await AsymptoticPerpetual.deploy();
       await asymptoticPerpetual.deployed();
-  
+
       // deploy token1155
       const Token = await ethers.getContractFactory("Token")
       const derivable1155 = await Token.deploy(
@@ -99,7 +99,7 @@ HLs.forEach(HALF_LIFE => {
         utr.address
       )
       await derivable1155.deployed()
-  
+
       // deploy ddl pool
       const oracle = ethers.utils.hexZeroPad(
         bn(quoteTokenIndex).shl(255).add(bn(300).shl(256 - 64)).add(uniswapPair.address).toHexString(),
@@ -121,7 +121,7 @@ HLs.forEach(HALF_LIFE => {
       const poolAddress = await poolFactory.computePoolAddress(params);
       let txSignerA = weth.connect(accountA);
       let txSignerB = weth.connect(accountB);
-  
+
       await txSignerA.deposit({
         value: '100000000000000000000000000000'
       })
@@ -134,7 +134,7 @@ HLs.forEach(HALF_LIFE => {
       await weth.transfer(poolAddress, numberToWei(1));
       await poolFactory.createPool(params);
       const derivablePool = await ethers.getContractAt("Pool", await poolFactory.computePoolAddress(params));
-  
+
       await time.increase(100);
       // deploy helper
       const DerivableHelper = await ethers.getContractFactory("Helper")
@@ -148,7 +148,7 @@ HLs.forEach(HALF_LIFE => {
       const R_ID = packId(0x00, derivablePool.address);
       const C_ID = packId(0x30, derivablePool.address);
       await weth.approve(derivablePool.address, '100000000000000000000000000');
-  
+
       txSignerA = weth.connect(accountA);
       txSignerB = weth.connect(accountB);
       await txSignerA.approve(derivablePool.address, '100000000000000000000000000');
@@ -159,7 +159,7 @@ HLs.forEach(HALF_LIFE => {
       await txSignerB.setApprovalForAll(derivablePool.address, true);
       txSignerA = derivablePool.connect(accountA);
       txSignerB = derivablePool.connect(accountB);
-  
+
       await txSignerA.exactIn(
         0x00,
         numberToWei(0.5),
@@ -167,7 +167,7 @@ HLs.forEach(HALF_LIFE => {
         '0x0000000000000000000000000000000000000000',
         accountA.address
       );
-  
+
       async function swapAndWait(period, waitingTime, amountA, amountB) {
         await txSignerA.exactIn(
           0x00,
@@ -185,7 +185,7 @@ HLs.forEach(HALF_LIFE => {
         );
         const aTokenAmount = await derivable1155.balanceOf(accountA.address, A_ID)
         const bTokenAmount = await derivable1155.balanceOf(accountB.address, B_ID)
-  
+
         const aFirstBefore = await txSignerA.callStatic.exactIn(
           0x10,
           aTokenAmount,
@@ -248,17 +248,24 @@ HLs.forEach(HALF_LIFE => {
           '0x0000000000000000000000000000000000000000',
           accountB.address
         )
-  
+
         const secondLongRate = Number(weiToNumber(aSecondAfter)) / Number(weiToNumber(aSecondBefore))
         const firstLongRate = Number(weiToNumber(aFirstAfter)) / Number(weiToNumber(aFirstBefore))
-  
+
         const secondShortRate = Number(weiToNumber(bSecondAfter)) / Number(weiToNumber(bSecondBefore))
         const firstShortRate = Number(weiToNumber(bFirstAfter)) / Number(weiToNumber(bFirstBefore))
-  
-        expect(secondLongRate).to.be.closeTo(firstLongRate, 0.000001)
-        expect(secondShortRate).to.be.closeTo(firstShortRate, 0.000001)
+        if (amountA.gt(amountB) & HALF_LIFE > 0) {
+          expect(aFirstBefore.sub(aFirstAfter)).gt(aSecondBefore.sub(aSecondAfter)).gt(0)
+          expect(secondShortRate).to.be.closeTo(firstShortRate, 0.000001)
+        } else if (amountA.lt(amountB) & HALF_LIFE > 0) {
+          expect(secondLongRate).to.be.closeTo(firstLongRate, 0.000001)
+          expect(bFirstBefore.sub(bFirstAfter)).gt(bSecondBefore.sub(bSecondAfter)).gt(0)
+        } else {
+          expect(secondLongRate).to.be.closeTo(firstLongRate, 0.000001)
+          expect(secondShortRate).to.be.closeTo(firstShortRate, 0.000001)
+        }
       }
-  
+
       async function instantSwapBackUTR(amountA, amountB) {
         // Acc A
         txSignerA = weth.connect(accountA);
@@ -343,7 +350,7 @@ HLs.forEach(HALF_LIFE => {
         const afterB = await weth.balanceOf(accountB.address)
         expect(beforeB.gte(afterB)).to.be.true
       }
-  
+
       async function groupSwapBack(amountA, amountB) {
         txSignerA = weth.connect(accountA);
         const beforeA = await weth.balanceOf(accountA.address)
@@ -423,7 +430,7 @@ HLs.forEach(HALF_LIFE => {
         console.log(changeOfB)
         // expect(amountB.gte(changeOfB)).to.be.true
       }
-  
+
       async function instantSwapBackNonUTR(amountA, amountB) {
         txSignerA = derivablePool.connect(accountA)
         await txSignerA.exactIn(
@@ -460,7 +467,7 @@ HLs.forEach(HALF_LIFE => {
         expect(amountA.gte(valueA)).to.be.true
         expect(amountB.gte(valueB)).to.be.true
       }
-  
+
       async function swapBackInAHalfLife(amountA, amountB, caseName) {
         txSignerA = derivablePool.connect(accountA)
         await txSignerA.exactIn(
@@ -478,7 +485,7 @@ HLs.forEach(HALF_LIFE => {
           '0x0000000000000000000000000000000000000000',
           accountB.address
         );
-  
+
         const aTokenAmount = await derivable1155.balanceOf(accountA.address, A_ID)
         const bTokenAmount = await derivable1155.balanceOf(accountB.address, B_ID)
         const valueABefore = await txSignerA.callStatic.exactIn(
@@ -520,7 +527,7 @@ HLs.forEach(HALF_LIFE => {
           `${caseName}: Value long should be half after halflife`
         )
       }
-  
+
       async function swapAndRedeemInHalfLife(period, amountA, amountB) {
         txSignerA = derivablePool.connect(accountA)
         txSignerB = derivablePool.connect(accountB)
@@ -557,10 +564,10 @@ HLs.forEach(HALF_LIFE => {
         if (period != 0 && HALF_LIFE > 0) {
           await time.increase(period * HALF_LIFE)
         }
-  
+
         const aBefore = await weth.balanceOf(accountA.address)
         const bBefore = await weth.balanceOf(accountB.address)
-  
+
         await txSignerA.exactIn(
           0x10,
           aTokenAmount,
@@ -575,7 +582,7 @@ HLs.forEach(HALF_LIFE => {
           '0x0000000000000000000000000000000000000000',
           accountB.address
         )
-  
+
         const aAfter = await weth.balanceOf(accountA.address)
         const bAfter = await weth.balanceOf(accountB.address)
         return {
@@ -585,34 +592,34 @@ HLs.forEach(HALF_LIFE => {
           shortFee: valueBBefore.sub(bAfter.sub(bBefore))
         }
       }
-  
+
       async function compareBalance(period) {
         const origin = await swapAndRedeemInHalfLife(1, numberToWei(0.5), numberToWei(0.5))
         const after = await swapAndRedeemInHalfLife(period, numberToWei(0.5), numberToWei(0.5))
         const expectRatio = (1 - 0.5) / (1 - 0.5 ** period)
-  
+
         expect(Number(weiToNumber(origin.longFee) / Number(weiToNumber(after.longFee)))).to.be.closeTo(expectRatio, 0.000001)
         expect(Number(weiToNumber(origin.shortFee) / Number(weiToNumber(after.shortFee)))).to.be.closeTo(expectRatio, 0.000001)
       }
-  
+
       async function compareMuchMoreLong(period) {
         const origin = await swapAndRedeemInHalfLife(1, numberToWei(2.5), numberToWei(0.5))
         const after = await swapAndRedeemInHalfLife(period, numberToWei(2.5), numberToWei(0.5))
         const expectRatio = (1 - 0.5) / (1 - 0.5 ** period)
-  
+
         expect(Number(weiToNumber(origin.longFee) / Number(weiToNumber(after.longFee)))).to.be.closeTo(expectRatio, 0.000001)
         expect(Number(weiToNumber(origin.shortFee) / Number(weiToNumber(after.shortFee)))).to.be.closeTo(expectRatio, 0.000001)
       }
-  
+
       async function compareMuchMoreShort(period) {
         const origin = await swapAndRedeemInHalfLife(1, numberToWei(0.5), numberToWei(2.5))
         const after = await swapAndRedeemInHalfLife(period, numberToWei(0.5), numberToWei(2.5))
         const expectRatio = (1 - 0.5) / (1 - 0.5 ** period)
-  
+
         expect(Number(weiToNumber(origin.longFee) / Number(weiToNumber(after.longFee)))).to.be.closeTo(expectRatio, 0.000001)
         expect(Number(weiToNumber(origin.shortFee) / Number(weiToNumber(after.shortFee)))).to.be.closeTo(expectRatio, 0.000001)
       }
-  
+
       return {
         C_ID,
         utr,
@@ -636,7 +643,7 @@ HLs.forEach(HALF_LIFE => {
         swapBackInAHalfLife
       }
     }
-  
+
     describe("Pool", function () {
       it("LP increase over time", async function () {
         const { swapAndRedeemInHalfLife, accountA, txSignerA, derivable1155, C_ID } = await loadFixture(deployDDLv2);
@@ -683,7 +690,7 @@ HLs.forEach(HALF_LIFE => {
           expect(Number(weiToNumber(after.long))).to.be.closeTo(1, 0.01)
           expect(Number(weiToNumber(after.short))).to.be.closeTo(1, 0.01)
         })
-  
+
         it("Decay same range, different time", async function () {
           const { swapAndRedeemInHalfLife } = await loadFixture(deployDDLv2);
           const before = await swapAndRedeemInHalfLife(0.1, numberToWei(1), numberToWei(1))
@@ -699,9 +706,9 @@ HLs.forEach(HALF_LIFE => {
             0.0000001
           )
         })
-  
+
       })
-  
+
       describe("Pool long > R/2:", function () {
         it("swap back after 1 halflife", async function () {
           const { swapBackInAHalfLife } = await loadFixture(deployDDLv2)
@@ -729,14 +736,15 @@ HLs.forEach(HALF_LIFE => {
           if (HALF_LIFE > 0)
             await time.increase(10 * HALF_LIFE)
           const after = await swapAndRedeemInHalfLife(0.1, numberToWei(2.5), numberToWei(0.5))
-          expect(before.long, 'Long Return').lt(after.long).gt(0)
+          expect(before.longFee, 'Long Return').gt(after.longFee).gt(0)
+          // expect(before.short, 'Short Return').gt(after.short).gt(0)
           expect(Number(weiToNumber(before.short))).to.be.closeTo(
             Number(weiToNumber(after.short)),
             0.0000001
           )
         })
       })
-  
+
       describe("Pool short > R/2:", function () {
         it("swap back after 1 halflife", async function () {
           const { swapBackInAHalfLife } = await loadFixture(deployDDLv2)
@@ -764,11 +772,12 @@ HLs.forEach(HALF_LIFE => {
           if (HALF_LIFE > 0)
             await time.increase(10 * HALF_LIFE)
           const after = await swapAndRedeemInHalfLife(0.1, numberToWei(0.5), numberToWei(2.5))
+          expect(before.shortFee, 'Short Return').gt(after.shortFee).gt(0)
+          // expect(before.long, 'Long Return').gt(after.long).gt(0)
           expect(Number(weiToNumber(before.long))).to.be.closeTo(
             Number(weiToNumber(after.long)),
             0.0000001
           )
-          expect(before.short, 'Short Return').lt(after.short).gt(0)
         })
         // TODO: Zergity verify this
         it("Group swap back", async function () {
