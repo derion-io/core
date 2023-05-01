20713efb6175eccf6b5996dcb109887530cfbf18
Zergity <zergity@gmail.com>
2023-05-01 19:18:46 +0700

    add Derivable author

--- contracts/tokens/ERC1155.sol
+++ contracts/tokens/ERC1155.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.8.0) (token/ERC1155/ERC1155.sol)
+// Derivable Contracts (tokens/ERC1155.sol)
 
 pragma solidity ^0.8.0;
 
--- contracts/tokens/ERC1155SupplyVirtual.sol
+++ contracts/tokens/ERC1155SupplyVirtual.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.6.0) (token/ERC1155/extensions/ERC1155Supply.sol)
+// Derivable Contracts (tokens/ERC1155SupplyVirtual.sol)
 
 pragma solidity ^0.8.0;
 
