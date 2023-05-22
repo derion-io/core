c4ea3ec9bdc7ed2730f934d76d091d74392c0de4
Ngo Quang Anh <anhnq82@gmail.com>
2023-05-22 14:20:11 +0700

    UT: add missing param

--- test/swap-helper.spec.js
+++ test/swap-helper.spec.js
@@ -185,6 +185,7 @@ describe("DDL v3", function () {
             halfLife: HALF_LIFE,
             minExpirationD: 0,
             minExpirationC: 0,
+            discountRate: 0
         }
         const poolAddress1 = await poolFactory.computePoolAddress(params1)
         // await weth.deposit({
