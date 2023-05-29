4042ab2705dad37de93b000eaab09c8de9b2dbf5
BerlinP <bknghi95@gmail.com>
2023-05-29 15:31:03 +0700

    Flashloan attack ut

---
+++ contracts/test/FlashloanAttack.sol
@@ -0,0 +1,56 @@
+pragma solidity 0.8.13;
+
+import "../interfaces/IPool.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+
+import "hardhat/console.sol";
+
+
+contract FlashloanAttack {
+  address immutable ROUTER;
+  address immutable POOL;
+
+  constructor(address router, address pool) {
+    ROUTER = router;
+    POOL = pool;
+  }
+
+  function attack(
+    ISwapRouter.ExactInputSingleParams calldata params,
+    address deriToken,
+    uint sideIn,
+    uint sideOut,
+    address helper,
+    bytes calldata payload,
+    uint32 expiration,
+    address payer,
+    address recipient
+  ) public {
+    IERC20(params.tokenIn).approve(ROUTER, type(uint).max);
+    IERC1155(deriToken).setApprovalForAll(POOL, true);
+    ISwapRouter(ROUTER).exactInputSingle(params);
+    (,uint amountOut) = IPool(POOL).swap(sideIn, sideOut, helper, payload, expiration, payer, recipient);
+  }
+
+   function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes memory
+    ) public virtual returns (bytes4) {
+        return this.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] memory,
+        uint256[] memory,
+        bytes memory
+    ) public virtual returns (bytes4) {
+        return this.onERC1155BatchReceived.selector;
+    }
+}
