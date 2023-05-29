c90ed92a2a4ba1c1a7a252b1b6b3181bfa6d4811
Zergity <zergity@gmail.com>
2023-05-29 15:32:58 +0700

    remove hardhat/console.sol and update license

--- contracts/test/FlashloanAttack.sol
+++ contracts/test/FlashloanAttack.sol
@@ -1,11 +1,10 @@
+// SPDX-License-Identifier: BSL-1.1
 pragma solidity 0.8.13;
 
 import "../interfaces/IPool.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-
-import "hardhat/console.sol";
 
 
 contract FlashloanAttack {
@@ -31,7 +30,7 @@ contract FlashloanAttack {
     IERC20(params.tokenIn).approve(ROUTER, type(uint).max);
     IERC1155(deriToken).setApprovalForAll(POOL, true);
     ISwapRouter(ROUTER).exactInputSingle(params);
-    (,uint amountOut) = IPool(POOL).swap(sideIn, sideOut, helper, payload, expiration, payer, recipient);
+    IPool(POOL).swap(sideIn, sideOut, helper, payload, expiration, payer, recipient);
   }
 
    function onERC1155Received(
