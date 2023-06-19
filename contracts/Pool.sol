f8273618315003ba0eece43e348ad9d4ab77fe2a
Zergity <zergity@gmail.com>
2023-06-19 11:22:42 +0700

    remove hardhat console

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -12,8 +12,6 @@ import "./interfaces/IPool.sol";
 import "./logics/Storage.sol";
 import "./logics/Events.sol";
 
-import "hardhat/console.sol";
-
 abstract contract Pool is IPool, Storage, Events, Constants {
     /// Immutables
     address internal immutable UTR;
