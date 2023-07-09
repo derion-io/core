513729e9503f2410be87d257e787c1bff45069f3
BerlinP <bknghi95@gmail.com>
2023-07-09 22:08:44 +0700

    case twap!=spot, deleverage short

--- test/view.test.js
+++ test/view.test.js
@@ -9,16 +9,20 @@ const { SIDE_R, SIDE_C, SIDE_A, SIDE_B } = require("./shared/constant")
 const { loadFixtureFromParams } = require("./shared/scenerios")
 chai.use(solidity)
 const expect = chai.expect
-const { numberToWei, packId, swapToSetPriceV3 } = require("./shared/utilities")
+const { numberToWei, packId, bn, swapToSetPriceMock } = require("./shared/utilities")
 
 describe("View", function () {
-  const fixture = loadFixtureFromParams([baseParams], {
-    callback: async function({derivablePools, uniswapPair}) {
+  const fixture = loadFixtureFromParams([{
+    ...baseParams,
+    halfLife: bn(19932680)
+  }], {
+    feeRate: 12,
+    callback: async function ({ derivablePools, uniswapPair }) {
       const pool = derivablePools[0]
       await pool.swap(
         SIDE_R,
         SIDE_C,
-        numberToWei(9995),
+        numberToWei(10),
         0
       )
 
@@ -29,31 +33,141 @@ describe("View", function () {
     }
   })
 
-  it("compute", async function () {
-    const { owner, weth, usdc, derivablePools, derivable1155, flashloan } = await loadFixture(fixture)
-    const pool = derivablePools[0]
+  describe("compute", function () {
+    it("Short, twap == spot", async function () {
+      const { owner, derivablePools, derivable1155 } = await loadFixture(fixture)
+      const pool = derivablePools[0]
 
-    const tokenBefore = await derivable1155.balanceOf(owner.address, packId(SIDE_B, pool.contract.address))
-    await pool.swap(
-      SIDE_R,
-      SIDE_B,
-      numberToWei(1),
-      0
-    )
-    
-    const tokenAfter = await derivable1155.balanceOf(owner.address, packId(SIDE_B, pool.contract.address))
-    const amountIn = tokenAfter.sub(tokenBefore)
-    
-    const { rA, sA, rB, sB, rC, sC } = await pool.contract.compute(derivable1155.address)
+      const tokenBefore = await derivable1155.balanceOf(owner.address, packId(SIDE_B, pool.contract.address))
+      await pool.swap(
+        SIDE_R,
+        SIDE_B,
+        numberToWei(1),
+        0
+      )
 
-    const amountOut = await pool.swap(
-      SIDE_B,
-      SIDE_R,
-      amountIn,
-      0,
-      { static: true }
-    )
+      const tokenAfter = await derivable1155.balanceOf(owner.address, packId(SIDE_B, pool.contract.address))
+      const amountIn = tokenAfter.sub(tokenBefore)
 
-    expect(amountIn.mul(rB).div(sB)).equal(amountOut)
+      const { rA, sA, rB, sB, rC, sC } = await pool.contract.compute(derivable1155.address)
+
+      const amountOut = await pool.swap(
+        SIDE_B,
+        SIDE_R,
+        amountIn,
+        0,
+        { static: true }
+      )
+
+      expect(amountIn.mul(rB).div(sB)).equal(amountOut)
+    })
+
+    it("Short, twap != spot", async function () {
+      const { owner, derivablePools, derivable1155, usdc, weth, uniswapPair } = await loadFixture(fixture)
+      const pool = derivablePools[0]
+
+      await swapToSetPriceMock({
+        quoteToken: usdc,
+        baseToken: weth,
+        uniswapPair,
+        targetSpot: 1500,
+        targetTwap: 1490
+      })
+
+      const tokenBefore = await derivable1155.balanceOf(owner.address, packId(SIDE_B, pool.contract.address))
+      await pool.swap(
+        SIDE_R,
+        SIDE_B,
+        numberToWei(1),
+        0
+      )
+
+      const tokenAfter = await derivable1155.balanceOf(owner.address, packId(SIDE_B, pool.contract.address))
+      const amountIn = tokenAfter.sub(tokenBefore)
+
+      const { rA, sA, rB, sB, rC, sC } = await pool.contract.compute(derivable1155.address)
+
+      const amountOut = await pool.swap(
+        SIDE_B,
+        SIDE_R,
+        amountIn,
+        0,
+        { static: true }
+      )
+
+      expect(amountIn.mul(rB).div(sB)).equal(amountOut)
+    })
+
+    it("Short, deleverage long", async function () {
+      const { owner, derivablePools, derivable1155, usdc, weth, uniswapPair } = await loadFixture(fixture)
+      const pool = derivablePools[0]
+
+      await swapToSetPriceMock({
+        quoteToken: usdc,
+        baseToken: weth,
+        uniswapPair,
+        targetSpot: 15000,
+        targetTwap: 15000
+      })
+
+      const tokenBefore = await derivable1155.balanceOf(owner.address, packId(SIDE_B, pool.contract.address))
+      await pool.swap(
+        SIDE_R,
+        SIDE_B,
+        numberToWei(1),
+        0
+      )
+
+      const tokenAfter = await derivable1155.balanceOf(owner.address, packId(SIDE_B, pool.contract.address))
+      const amountIn = tokenAfter.sub(tokenBefore)
+
+      const { rA, sA, rB, sB, rC, sC } = await pool.contract.compute(derivable1155.address)
+
+      const amountOut = await pool.swap(
+        SIDE_B,
+        SIDE_R,
+        amountIn,
+        0,
+        { static: true }
+      )
+
+      expect(amountIn.mul(rB).div(sB)).equal(amountOut)
+    })
+
+    it("Short, deleverage short", async function () {
+      const { owner, derivablePools, derivable1155, usdc, weth, uniswapPair } = await loadFixture(fixture)
+      const pool = derivablePools[0]
+
+      await swapToSetPriceMock({
+        quoteToken: usdc,
+        baseToken: weth,
+        uniswapPair,
+        targetSpot: 150,
+        targetTwap: 150
+      })
+
+      const tokenBefore = await derivable1155.balanceOf(owner.address, packId(SIDE_B, pool.contract.address))
+      await pool.swap(
+        SIDE_R,
+        SIDE_B,
+        numberToWei(1),
+        0
+      )
+
+      const tokenAfter = await derivable1155.balanceOf(owner.address, packId(SIDE_B, pool.contract.address))
+      const amountIn = tokenAfter.sub(tokenBefore)
+
+      const { rA, sA, rB, sB, rC, sC } = await pool.contract.compute(derivable1155.address)
+
+      const amountOut = await pool.swap(
+        SIDE_B,
+        SIDE_R,
+        amountIn,
+        0,
+        { static: true }
+      )
+
+      expect(amountIn.mul(rB).div(sB)).equal(amountOut)
+    })
   })
 })
