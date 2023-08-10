a19cea797ee4bf7af2f4d1bae7b5af63cc120f83
BerlinP <bknghi95@gmail.com>
2023-08-10 22:05:59 +0700

    Fix UT

--- monkey/premium.js
+++ monkey/premium.js
@@ -22,25 +22,25 @@ function toHalfLife(dailyRate) {
 
 const configs = [
     {
-        interestRate: 0.04,
-        premiumRate: 0.5
+        interestRate: 0.004,
+        premiumRate: 0.01
     },
     {
-        interestRate: 0.02,
-        premiumRate: 0.02
+        interestRate: 0.002,
+        premiumRate: 0.002
     },
     {
         interestRate: 0,
         premiumRate: 0.01
     },
     {
-        interestRate: 0.04,
+        interestRate: 0.004,
         premiumRate: 0
     }
 ]
 
 configs.forEach(config => {
-    describe(`Monkey Test: Interest rate ${config.interestRate}%, Premium ${config.premiumRate}%`, function () {
+    describe(`Monkey Test: Interest rate ${config.interestRate * 100}%, Premium ${config.premiumRate * 100}%`, function () {
         const fixture = loadFixtureFromParams([{
             ...baseParams,
             halfLife: bn(toHalfLife(config.interestRate)),
@@ -48,7 +48,7 @@ configs.forEach(config => {
             mark: bn('13179171373343029902768196957842336318319')
         }], {
             logicName: 'View',
-            initReserved: 300,
+            initReserved: 3,
             feeRate: 0
         })
 
@@ -95,29 +95,51 @@ configs.forEach(config => {
             expect(rC1Num/expectedRC1Num).to.be.closeTo(1, 1e-3)
         }
     
-        it('Test', async function () {
+        it('Test Balance', async function () {
             const { derivablePools, derivable1155, uniswapPair, usdc, weth } = await loadFixture(fixture)
             const pool = derivablePools[0]
+            await pool.swap(
+                SIDE_R,
+                SIDE_C,
+                numberToWei(99)
+            )
+
+            await pool.swap(
+                SIDE_R,
+                SIDE_B,
+                numberToWei(99)
+            )
+
+            await pool.swap(
+                SIDE_R,
+                SIDE_A,
+                numberToWei(99)
+            )
+
+            const { rA, rB, rC } = await pool.contract.compute(derivable1155.address)
+            const rANum = Number(weiToNumber(rA))
+            const rBNum = Number(weiToNumber(rB))
+            const rCNum = Number(weiToNumber(rC))
+
+            const startTime = await time.latest()
 
             const A_ID = packId(SIDE_A, pool.contract.address);
             const B_ID = packId(SIDE_B, pool.contract.address);
             const C_ID = packId(SIDE_C, pool.contract.address);
 
             let currentPrice = 1500
-            for (let i = 0; i < 1000; i++) {
+            for (let i = 0; i < 500; i++) {
                 const rand = Math.random()
                 if (rand < 2 / 3) { //swap
                     const isBuy = Math.random() < 0.5
                     // Choose side
                     const sideRand = Math.random()
-                    let amount = 0.1 + 3 * Math.random()
+                    let amount = 0.0001
                     let side = SIDE_A
-                    if (sideRand < 4 / 9) {
+                    if (sideRand < 1 / 2) {
                         side = SIDE_B
-                    } else if (sideRand < 5 / 9) {
-                        side = SIDE_C
                     }
-                    console.log(`${i} - ${isBuy ? 'Buy' : 'Sell'} - ${side} - ${amount}`)
+                    // console.log(`${i} - ${isBuy ? 'Buy' : 'Sell'} - ${side} - ${amount}`)
                     if (isBuy) {
                         await pool.swap(
                             SIDE_R,
@@ -125,10 +147,7 @@ configs.forEach(config => {
                             numberToWei(amount),
                         )
                     } else {
-                        if (side == SIDE_C) {
-                            const supplyC = Number(weiToNumber(await derivable1155.totalSupply(C_ID)))
-                            amount = (supplyC * amount / 10)
-                        } else if (side == SIDE_A) {
+                        if (side == SIDE_A) {
                             const supplyA = Number(weiToNumber(await derivable1155.totalSupply(A_ID)))
                             amount = (supplyA * amount / 10)
                         } else {
@@ -144,7 +163,7 @@ configs.forEach(config => {
     
                 } else { //change price
                     const targetPrice = 1500 + 5 - 10 * Math.random()
-                    console.log(`${i} - change price - from ${currentPrice} - to ${targetPrice}`)
+                    // console.log(`${i} - change price - from ${currentPrice} - to ${targetPrice}`)
                     swapToSetPriceMock({
                         quoteToken: usdc,
                         baseToken: weth,
@@ -160,7 +179,182 @@ configs.forEach(config => {
                 }
             }
     
-            await compareWithInterest(pool, derivable1155)
+            await pool.swap(
+                SIDE_R,
+                SIDE_C,
+                1
+            )
+
+            const endTime = await time.latest()
+            const elapsedDay = (endTime - startTime) / SECONDS_PER_DAY
+
+            const interestRate = (1 - config.interestRate ) ** elapsedDay
+            const rADecayed = rANum * interestRate
+            const rBDecayed = rBNum * interestRate
+            const rCDecayed = rCNum + (rANum + rBNum) * (1 - interestRate)
+
+            const { rA: rA1, rB: rB1, rC: rC1 } = await pool.contract.compute(derivable1155.address)
+            const rA1Num = Number(weiToNumber(rA1))
+            const rB1Num = Number(weiToNumber(rB1))
+            const rC1Num = Number(weiToNumber(rC1))
+    
+            let expectedRA1Num
+            let expectedRB1Num
+            let expectedRC1Num
+    
+            const premiumRate = (1 - (1 - config.premiumRate) ** elapsedDay)
+            if (rADecayed >= rBDecayed) {
+                const expectedPremium = (rADecayed - rBDecayed) * premiumRate
+                expectedRA1Num = rADecayed - expectedPremium
+                expectedRB1Num = rBDecayed + expectedPremium * rBDecayed / (rBDecayed + rCDecayed)
+                expectedRC1Num = rCDecayed + expectedPremium * rCDecayed / (rBDecayed + rCDecayed)
+            } else {
+                const expectedPremium = (rBDecayed - rADecayed) * premiumRate
+                expectedRB1Num = rBDecayed - expectedPremium
+                expectedRA1Num = rADecayed + expectedPremium * rADecayed / (rADecayed + rCDecayed)
+                expectedRC1Num = rCDecayed + expectedPremium * rCDecayed / (rADecayed + rCDecayed)
+            }
+    
+            expect(rA1Num/expectedRA1Num).to.be.closeTo(1, 0.1)
+            expect(rB1Num/expectedRB1Num).to.be.closeTo(1, 0.1)
+            expect(rC1Num/expectedRC1Num).to.be.closeTo(1, 0.1)
+        })
+
+        it('Test Imbalance: rA 300, rB 100, rC 200', async function () {
+            const { derivablePools, derivable1155, uniswapPair, usdc, weth } = await loadFixture(fixture)
+            const pool = derivablePools[0]
+
+            await pool.swap(
+                SIDE_R,
+                SIDE_C,
+                numberToWei(199)
+            )
+
+            await pool.swap(
+                SIDE_R,
+                SIDE_A,
+                numberToWei(299)
+            )
+
+            await pool.swap(
+                SIDE_R,
+                SIDE_B,
+                numberToWei(99)
+            )
+
+            const { rA, rB, rC } = await pool.contract.compute(derivable1155.address)
+            const rANum = Number(weiToNumber(rA))
+            const rBNum = Number(weiToNumber(rB))
+            const rCNum = Number(weiToNumber(rC))
+
+            let rAExpected = rANum
+            let rBExpected = rBNum
+            let rCExpected = rCNum
+
+            const startTime = await time.latest()
+
+            const A_ID = packId(SIDE_A, pool.contract.address);
+            const B_ID = packId(SIDE_B, pool.contract.address);
+            const C_ID = packId(SIDE_C, pool.contract.address);
+
+            let currentPrice = 1500
+            let lastTime = await time.latest()
+            for (let i = 0; i < 500; i++) {
+                const rand = Math.random()
+                const isSwap = rand < 2 / 3
+                if (isSwap) { //swap
+                    const isBuy = Math.random() < 0.5
+                    // Choose side
+                    const sideRand = Math.random()
+                    let amount = 0.0001
+                    let side = SIDE_A
+                    if (sideRand < 1 / 2) {
+                        side = SIDE_B
+                    }
+                    console.log(`${i} - ${isBuy ? 'Buy' : 'Sell'} - ${side} - ${amount}`)
+                    if (isBuy) {
+                        await pool.swap(
+                            SIDE_R,
+                            side,
+                            numberToWei(amount),
+                        )
+                    } else {
+                        if (side == SIDE_A) {
+                            const supplyA = Number(weiToNumber(await derivable1155.totalSupply(A_ID)))
+                            amount = (supplyA * amount / 10)
+                        } else {
+                            const supplyB = Number(weiToNumber(await derivable1155.totalSupply(B_ID)))
+                            amount = (supplyB * amount / 10)
+                        }
+                        await pool.swap(
+                            side,
+                            SIDE_R,
+                            numberToWei(amount),
+                        )
+                    }
+    
+                } else { //change price
+                    const targetPrice = 1500 + 5 - 10 * Math.random()
+                    // console.log(`${i} - change price - from ${currentPrice} - to ${targetPrice}`)
+                    swapToSetPriceMock({
+                        quoteToken: usdc,
+                        baseToken: weth,
+                        uniswapPair,
+                        targetSpot: targetPrice,
+                        targetTwap: targetPrice
+                    })
+                    currentPrice = targetPrice
+                }
+                const wait = Math.round(Math.random() * SECONDS_PER_DAY)
+                if (wait > 0) {
+                    await time.increase(wait)
+                }
+                if (isSwap) {
+                    const current = await time.latest()
+
+                    const elapsedDay = (current - lastTime) / SECONDS_PER_DAY
+                    const interestRate = (1 - config.interestRate ) ** elapsedDay
+
+
+                    const rADecayed = rAExpected * interestRate
+                    const rBDecayed = rBExpected * interestRate
+                    const rCDecayed = rCExpected + (rAExpected + rBExpected) * (1 - interestRate)
+
+                    const premiumRate = (1 - (1 - config.premiumRate) ** elapsedDay)
+                    if (rADecayed > rBDecayed) {
+                        const expectedPremium = (rADecayed - rBDecayed) * premiumRate
+                        rAExpected = rADecayed - expectedPremium
+                        rBExpected = rBDecayed + expectedPremium * rBDecayed / (rBDecayed + rCDecayed)
+                        rCExpected = rCDecayed + expectedPremium * rCDecayed / (rBDecayed + rCDecayed)
+                    } else {
+                        const expectedPremium = (rBDecayed - rADecayed) * premiumRate
+                        rBExpected = rBDecayed - expectedPremium
+                        rAExpected = rADecayed + expectedPremium * rADecayed / (rADecayed + rCDecayed)
+                        rCExpected = rCDecayed + expectedPremium * rCDecayed / (rADecayed + rCDecayed)
+                    }
+
+                    lastTime = current
+                }
+            }
+    
+            await pool.swap(
+                SIDE_R,
+                SIDE_C,
+                1
+            )
+
+            const { rA: rA1, rB: rB1, rC: rC1 } = await pool.contract.compute(derivable1155.address)
+            const rA1Num = Number(weiToNumber(rA1))
+            const rB1Num = Number(weiToNumber(rB1))
+            const rC1Num = Number(weiToNumber(rC1))
+    
+            console.log(rA1Num, rAExpected)
+            console.log(rB1Num, rBExpected)
+            console.log(rC1Num, rCExpected)
+    
+            expect(rA1Num/rAExpected).to.be.closeTo(1, 0.1)
+            expect(rB1Num/rBExpected).to.be.closeTo(1, 0.1)
+            expect(rC1Num/rCExpected).to.be.closeTo(1, 0.1)
         })
     })
 })
