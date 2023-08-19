5845b4d69c361b3a837d6fb5034253d2dd461f20
BerlinP <bknghi95@gmail.com>
2023-08-19 19:33:03 +0700

    Token price view

---
+++ contracts/support/TokenPriceView.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: BSL-1.1
+pragma solidity ^0.8.0;
+
+import '@uniswap/v3-core/contracts/libraries/FullMath.sol';
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+
+contract TokenPriceView {
+    function fetchMarketBatch(
+        address[] calldata tokens,
+        address factory,
+        address[] calldata otherTokens,
+        address weth,
+        address usd
+    ) external view returns (uint[] memory sqrtPriceX96) {
+        sqrtPriceX96 = new uint[](tokens.length);
+        for (uint i = 0; i < tokens.length; i++) {
+            sqrtPriceX96[i] = fetchMarket(tokens[i], factory, otherTokens, weth, usd);
+        }
+    }
+
+    function fetchMarket (
+        address token,
+        address factory,
+        address[] calldata otherTokens,
+        address weth,
+        address usd
+    ) public view returns (uint sqrtPriceX96) {
+        uint16[3] memory FEE = [500, 3000, 10000];
+        uint curReserve = 0;
+        address bestOtherToken;
+        for (uint i = 0; i < FEE.length; i++) {
+            for (uint j = 0; j < otherTokens.length; j++) {
+                address pool = IUniswapV3Factory(factory).getPool(token, otherTokens[j], uint24(FEE[i]));
+                if (pool != address(0)) {
+                    uint tokenReserve = IERC20(token).balanceOf(pool);
+                    if (curReserve < tokenReserve) {
+
+                        curReserve = tokenReserve;
+                        bestOtherToken = otherTokens[j];
+                        sqrtPriceX96 = _getTokenPrice(pool, token, otherTokens[j]);
+                    }
+                }
+            } 
+        }
+
+        
+        if (bestOtherToken == weth) {
+            address pool = IUniswapV3Factory(factory).getPool(weth, usd, 500);
+            uint wethSqrtPriceX96 = _getTokenPrice(pool, weth, usd);
+            sqrtPriceX96 = FullMath.mulDiv(sqrtPriceX96, wethSqrtPriceX96, 1 << 96);
+        }
+    }
+
+    function _getTokenPrice(address pool, address token, address otherToken) internal view returns (uint sqrtPriceX96) {
+        
+        (uint160 price,,,,,,) = IUniswapV3Pool(pool).slot0();
+
+        sqrtPriceX96 = uint(price);
+        if (token > otherToken) {
+            sqrtPriceX96 = (1 << 192) / sqrtPriceX96;
+
+        }
+    }
+}
