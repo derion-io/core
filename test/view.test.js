be124bb474f4a5fb14f9f6d669af11c5de895026
BerlinP <bknghi95@gmail.com>
2023-09-16 16:55:22 +0700

    Compute long time with no tx

--- test/view.test.js
+++ test/view.test.js
@@ -1,44 +1,65 @@
 const {
-  loadFixture, time,
-} = require("@nomicfoundation/hardhat-network-helpers")
-const chai = require("chai")
-const { solidity } = require("ethereum-waffle")
-const { ethers } = require("hardhat")
-const { baseParams } = require("./shared/baseParams")
-const { SIDE_R, SIDE_C, SIDE_A, SIDE_B } = require("./shared/constant")
-const { loadFixtureFromParams } = require("./shared/scenerios")
-chai.use(solidity)
-const expect = chai.expect
-const { numberToWei, packId, bn, swapToSetPriceMock, weiToNumber } = require("./shared/utilities")
+  loadFixture,
+  time,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const chai = require("chai");
+const { solidity } = require("ethereum-waffle");
+const { ethers } = require("hardhat");
+const { baseParams } = require("./shared/baseParams");
+const { SIDE_R, SIDE_C, SIDE_A, SIDE_B } = require("./shared/constant");
+const { loadFixtureFromParams } = require("./shared/scenerios");
+chai.use(solidity);
+const expect = chai.expect;
+const {
+  numberToWei,
+  packId,
+  bn,
+  swapToSetPriceMock,
+  weiToNumber,
+} = require("./shared/utilities");
 
-const ELLAPSED_TIME = 86400 * 365
+const ELLAPSED_TIME = 86400 * 365;
 
 describe("View", function () {
-  const fixture = loadFixtureFromParams([{
-    ...baseParams,
-    halfLife: bn(19932680),
-  }], {
-    logicName: "View",
-    feeRate: 5,
-    callback: async function ({ derivablePools, uniswapPair }) {
-      const pool = derivablePools[0]
-      await pool.swap(
-        SIDE_R,
-        SIDE_C,
-        numberToWei(10),
-      )
+  const fixture = loadFixtureFromParams(
+    [
+      {
+        ...baseParams,
+        halfLife: bn(19932680),
+      },
+    ],
+    {
+      logicName: "View",
+      feeRate: 5,
+      callback: async function ({ derivablePools, uniswapPair }) {
+        const pool = derivablePools[0];
+        await pool.swap(SIDE_R, SIDE_C, numberToWei(10));
 
-      const FlashloanAttack = await ethers.getContractFactory('FlashloanAttack');
-      const flashloan = await FlashloanAttack.deploy(uniswapPair.address, pool.contract.address)
+        const FlashloanAttack = await ethers.getContractFactory(
+          "FlashloanAttack"
+        );
+        const flashloan = await FlashloanAttack.deploy(
+          uniswapPair.address,
+          pool.contract.address
+        );
 
-      return { flashloan }
+        return { flashloan };
+      },
     }
-  })
+  );
 
   describe("compute", function () {
     async function testCompute(side, price, closeTo = false) {
-      const { owner, derivablePools, derivable1155, usdc, weth, uniswapPair, feeRate } = await loadFixture(fixture)
-      const pool = derivablePools[0]
+      const {
+        owner,
+        derivablePools,
+        derivable1155,
+        usdc,
+        weth,
+        uniswapPair,
+        feeRate,
+      } = await loadFixture(fixture);
+      const pool = derivablePools[0];
 
       if (price)
         await swapToSetPriceMock({
@@ -46,91 +67,129 @@ describe("View", function () {
           baseToken: weth,
           uniswapPair,
           targetSpot: price.spot,
-          targetTwap: price.twap
-        })
+          targetTwap: price.twap,
+        });
 
-      const tokenBefore = await derivable1155.balanceOf(owner.address, packId(side, pool.contract.address))
-      await pool.swap(
-        SIDE_R,
-        side,
-        numberToWei(1),
-      )
+      const tokenBefore = await derivable1155.balanceOf(
+        owner.address,
+        packId(side, pool.contract.address)
+      );
+      await pool.swap(SIDE_R, side, numberToWei(1));
 
-      const tokenAfter = await derivable1155.balanceOf(owner.address, packId(side, pool.contract.address))
-      const amountIn = tokenAfter.sub(tokenBefore)
+      const tokenAfter = await derivable1155.balanceOf(
+        owner.address,
+        packId(side, pool.contract.address)
+      );
+      const amountIn = tokenAfter.sub(tokenBefore);
 
-      await time.increase(ELLAPSED_TIME)
+      await time.increase(ELLAPSED_TIME);
 
-      const { rA, sA, rB, sB, rC, sC } = await pool.contract.compute(derivable1155.address, feeRate)
+      const { rA, sA, rB, sB, rC, sC } = await pool.contract.compute(
+        derivable1155.address,
+        feeRate
+      );
 
-      let r = rB
-      let s = sB
+      let r = rB;
+      let s = sB;
       if (side == SIDE_A) {
-        r = rA
-        s = sA
+        r = rA;
+        s = sA;
       } else if (side == SIDE_C) {
-        r = rC
-        s = sC
+        r = rC;
+        s = sC;
       }
 
-      const amountOut = await pool.swap(
-        side,
-        SIDE_R,
-        amountIn,
-        { static: true }
-      )
-      if (closeTo) 
-        expect(Number(weiToNumber(amountIn.mul(r).div(s)))).closeTo(Number(weiToNumber(amountOut)), 1e-17)
+      const amountOut = await pool.swap(side, SIDE_R, amountIn, {
+        static: true,
+      });
+      if (closeTo)
+        expect(Number(weiToNumber(amountIn.mul(r).div(s)))).closeTo(
+          Number(weiToNumber(amountOut)),
+          1e-17
+        );
       else
-        expect(amountIn.mul(r).div(s)).gte(amountOut.sub(2)).lte(amountOut.add(2))
+        expect(amountIn.mul(r).div(s))
+          .gte(amountOut.sub(2))
+          .lte(amountOut.add(2));
     }
 
+    it("Compute long time with no tx", async function () {
+      const {
+        accountA,
+        derivablePools,
+        derivable1155,
+        feeRate,
+      } = await loadFixture(fixture);
+      const pool = derivablePools[0].connect(accountA);
+      await pool.swap(SIDE_R, SIDE_A, numberToWei(1));
+      await pool.swap(SIDE_R, SIDE_B, numberToWei(1));
+      const balanceA = await derivable1155.balanceOf(
+        accountA.address,
+        packId(SIDE_A, pool.contract.address)
+      );
+      const balanceB = await derivable1155.balanceOf(
+        accountA.address,
+        packId(SIDE_B, pool.contract.address)
+      );
+
+      await time.increase(365 * 86400)
+
+      const [{ rA, sA, rB, sB }, amountOutA, amountOutB] = await Promise.all([
+        pool.contract.compute(derivable1155.address, feeRate),
+        pool.swap(SIDE_A, SIDE_R, balanceA, { static: true }),
+        pool.swap(SIDE_B, SIDE_R, balanceB, { static: true }),
+      ]);
+
+      expect(balanceA.mul(rA).div(sA)).to.eq(amountOutA);
+      expect(balanceB.mul(rB).div(sB)).to.eq(amountOutB);
+    });
+
     it("Short, twap == spot", async function () {
-      await testCompute(SIDE_B, null)
-    })
+      await testCompute(SIDE_B, null);
+    });
 
     it("Short, twap != spot", async function () {
-      await testCompute(SIDE_B, {spot: 1500, twap: 1490})
-    })
+      await testCompute(SIDE_B, { spot: 1500, twap: 1490 });
+    });
 
     it("Short, deleverage long", async function () {
-      await testCompute(SIDE_B, {spot: 15000, twap: 15000})
-    })
+      await testCompute(SIDE_B, { spot: 15000, twap: 15000 });
+    });
 
     it("Short, deleverage short", async function () {
-      await testCompute(SIDE_B, {spot: 150, twap: 150})
-    })
+      await testCompute(SIDE_B, { spot: 150, twap: 150 });
+    });
 
     it("Long, twap == spot", async function () {
-      await testCompute(SIDE_A, null)
-    })
+      await testCompute(SIDE_A, null);
+    });
 
     it("Long, twap != spot", async function () {
-      await testCompute(SIDE_A, {spot: 1500, twap: 1490})
-    })
+      await testCompute(SIDE_A, { spot: 1500, twap: 1490 });
+    });
 
     it("Long, deleverage long", async function () {
-      await testCompute(SIDE_A, {spot: 15000, twap: 15000})
-    })
+      await testCompute(SIDE_A, { spot: 15000, twap: 15000 });
+    });
 
     it("Long, deleverage short", async function () {
-      await testCompute(SIDE_A, {spot: 150, twap: 150})
-    })
+      await testCompute(SIDE_A, { spot: 150, twap: 150 });
+    });
 
     it("LP, twap == spot", async function () {
-      await testCompute(SIDE_C, null, true)
-    })
+      await testCompute(SIDE_C, null, true);
+    });
 
     it("LP, twap != spot", async function () {
-      await testCompute(SIDE_C, {spot: 1500, twap: 1490}, true)
-    })
+      await testCompute(SIDE_C, { spot: 1500, twap: 1490 }, true);
+    });
 
     it("LP, deleverage long", async function () {
-      await testCompute(SIDE_C, {spot: 15000, twap: 15000})
-    })
+      await testCompute(SIDE_C, { spot: 15000, twap: 15000 });
+    });
 
     it("LP, deleverage short", async function () {
-      await testCompute(SIDE_C, {spot: 150, twap: 150})
-    })
-  })
-})
+      await testCompute(SIDE_C, { spot: 150, twap: 150 });
+    });
+  });
+});
