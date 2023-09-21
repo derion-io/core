22cc6b731c9c9f96cbb04fd5cde0316a240fb928
Zergity <zergity@gmail.com>
2023-09-21 10:36:56 +0700

    add deployPool script

---
+++ scripts/deployPool.js
@@ -0,0 +1,198 @@
+const { ethers } = require("hardhat")
+require('dotenv').config()
+const { bn, feeToOpenRate, toHalfLife } = require("../test/shared/utilities")
+const { calculateInitParamsFromPrice } = require("../test/shared/AsymptoticPerpetual")
+const { AddressZero } = ethers.constants
+const uniV3Pool = require("./compiled/UniswapV3Pool.json");
+const { JsonRpcProvider } = ethers.providers
+
+const pe = (x) => ethers.utils.parseEther(String(x))
+
+const SECONDS_PER_HOUR = 60 * 60
+const SECONDS_PER_DAY = SECONDS_PER_HOUR * 24
+
+const Q256M = bn(1).shl(256).sub(1)
+
+function compoundRate(r, k) {
+    return 1-((1-r)**k)
+}
+
+const chainID = 42161
+const SCAN_API_KEY = {
+    42161: process.env.SCAN_API_KEY_42161,
+}
+
+const settings = {
+    // pairAddress: '0xC31E54c7a869B9FcBEcc14363CF510d1c41fa443',
+    pairAddress: '0x8d76e9c2bd1adde00a3dcdc315fcb2774cb3d1d6',
+    power: 10,
+    interestRate: 0.03/100,
+    premiumRate: 10/100,
+    reserveToken: undefined, // use the WETH
+    MATURITY: 60 * 60 * 12,
+    vesting: 60,
+    closingFeeDuration: 24*60*60,
+    closingFee: 0.3/100,
+    // R: 0.0001, // init liquidity
+}
+
+async function deploy(settings) {
+    const configs = await fetch(
+        `https://raw.githubusercontent.com/derivable-labs/configs/dev/${chainID}/network.json`
+    ).then(res => res.json())
+
+    const provider = new JsonRpcProvider(configs.rpc, chainID)
+    const deployer = new ethers.Wallet(process.env.DEPLOYER_KEY, provider);
+    const uniswapPair = new ethers.Contract(settings.pairAddress, uniV3Pool.abi, provider)
+
+    const [
+        token0,
+        token1,
+        slot0,
+    ] = await Promise.all([
+        uniswapPair.callStatic.token0(),
+        uniswapPair.callStatic.token1(),
+        uniswapPair.callStatic.slot0(),
+    ])
+
+    // detect QTI
+    let QTI
+    if (QTI == null && configs.stablecoins.includes(token0)) {
+        QTI = 0
+    }
+    if (QTI == null && configs.stablecoins.includes(token1)) {
+        QTI = 1
+    }
+    if (QTI == null && configs.wrappedTokenAddress == token0) {
+        QTI = 0
+    }
+    if (QTI == null && configs.wrappedTokenAddress == token1) {
+        QTI = 1
+    }
+    if (QTI == null) {
+        throw new Error('unable to detect QTI')
+    }
+
+    // detect WINDOW
+    // get the block a day before
+    const now = Math.floor(new Date().getTime() / 1000)
+    const EPOCH = 500 * 60
+    const anEpochAgo = now - EPOCH
+    const blockEpochAgo = await fetch(
+        `${configs.scanAPI}?module=block&action=getblocknobytime&timestamp=${anEpochAgo}&closest=before&apikey=${SCAN_API_KEY[chainID]}`
+    ).then(x => x.json()).then(x => Number(x?.result))
+
+    const logs = await fetch(
+        `${configs.scanAPI}?module=logs&action=getLogs&address=${settings.pairAddress}&topic0=0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67&fromBlock=${blockEpochAgo}&apikey=${SCAN_API_KEY[chainID]}`
+    ).then(x => x.json()).then(x => x?.result)
+
+    if (!logs?.length) {
+        throw new Error('no transaction for a whole day')
+    }
+
+    const txFreq = EPOCH / logs.length
+    const WINDOW = Math.ceil(Math.sqrt(txFreq / 60)) * 60
+    console.log('WINDOW', WINDOW / 60, 'min(s)')
+
+    const ORACLE = ethers.utils.hexZeroPad(
+        bn(QTI).shl(255).add(bn(WINDOW).shl(256 - 64)).add(settings.pairAddress).toHexString(),
+        32,
+    )
+
+    let MARK = slot0.sqrtPriceX96.shl(32)
+    if (QTI == 0) {
+        MARK = Q256M.div(MARK)
+    }
+
+    // TODO: get token0 and token1 decimals
+    const PRICE = MARK.mul(MARK)
+    if (PRICE.lt(Q256M)) {
+        console.log('MARK', 1 / Q256M.div(PRICE).toNumber())
+    }
+    console.log('MARK', PRICE.div(Q256M).toNumber())
+
+    const DAILY_INTEREST_RATE = compoundRate(settings.interestRate, settings.power)
+    const DAILY_PREMIUM_RATE = compoundRate(settings.premiumRate, settings.power)
+    const INTEREST_HL = toHalfLife(DAILY_INTEREST_RATE)
+    const PREMIUM_HL = toHalfLife(DAILY_PREMIUM_RATE)
+
+    console.log('INTEREST_HL', (INTEREST_HL / SECONDS_PER_DAY).toFixed(2), 'day(s)')
+    console.log('PREMIUM_HL', (PREMIUM_HL / SECONDS_PER_DAY).toFixed(2), 'day(s)')
+
+    const config = {
+        FETCHER: AddressZero,
+        ORACLE,
+        TOKEN_R: settings.reserveToken ?? configs.wrappedTokenAddress,
+        MARK,
+        K: bn(settings.power * 2),
+        INTEREST_HL,
+        PREMIUM_HL,
+        MATURITY: settings.closingFeeDuration,
+        MATURITY_VEST: settings.vesting,
+        MATURITY_RATE: feeToOpenRate(settings.closingFee ?? 0),
+        OPEN_RATE: feeToOpenRate(settings.openingFee ?? 0),
+    }
+
+    // console.log(config)
+
+    // Create Pool
+    // const poolFactory = await ethers.getContractAt("contracts/PoolFactory.sol:PoolFactory", configs.derivable.poolFactory, deployer)
+    // const poolAddress = await poolFactory.callStatic.createPool(config)
+
+    // init the pool
+    const R = pe(settings.R ?? 0.0001)
+    const state = await calculateInitParamsFromPrice(config, MARK, R)
+
+    const helper = await ethers.getContractAt("contracts/support/Helper.sol:Helper", configs.derivable.helper ?? configs.derivable.stateCalHelper, deployer)
+
+    const gas = await helper.estimateGas.createPool(
+        config,
+        state,
+        configs.derivable.poolFactory,
+        { value: R },
+    )
+
+    console.log('Estimated Gas:', gas.toNumber().toLocaleString())
+
+    const params = [
+        config,
+        state,
+        configs.derivable.poolFactory,
+        { value: R, gasLimit: gas.mul(3).div(2) },
+    ]
+
+    const poolAddress = await helper.callStatic.createPool(...params)
+    console.log('New Pool Address:', poolAddress)
+
+    console.log('> Enter to deploy, Ctrl-C to stop.');
+
+    await waitForKey()
+
+    console.log('Sending tx...')
+
+    const tx = await helper.createPool(...params)
+
+    console.log('Waiting for tx receipt...', tx.hash)
+    
+    const rec = await tx.wait()
+
+    console.log(rec)
+}
+
+function waitForKey(keyCode = 10) {
+    return new Promise(resolve => {
+        process.stdin.on('data',function (chunk) {
+            if (chunk[0] === keyCode) {
+                resolve();
+                process.stdin.pause();
+            }
+        });
+    });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+deploy(settings).catch((error) => {
+    console.error(error)
+    process.exitCode = 1
+})
