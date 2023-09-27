79c67299d661a201a2018a1fe67a67ea9fb37ec8
Zergity <zergity@gmail.com>
2023-09-27 18:35:37 +0700

    double ranges halflife

--- test/edge-cases.test.ts
+++ test/edge-cases.test.ts
@@ -14,42 +14,49 @@ const {
   packId,
 } = require("./shared/utilities");
 
-describe("Edge cases", function () {
-  describe("HL = 1, premiumHL = 1", function () {
-    const fixture = loadFixtureFromParams([
-      {
-        ...baseParams,
-        halfLife: bn(1),
-        premiumHL: bn(1),
-      },
-    ]);
+const INTEREST_HLS = [0, 1, 60, 60 * 60, 1024]
+const PREMIUM_HLS = [0, 1, 60, 60 * 60, 1024]
 
-    async function openAndClosePosition(side, amount, period) {
-      const { derivablePools, derivable1155, accountA } = await loadFixture(
-        fixture
-      );
-      const pool = derivablePools[0].connect(accountA);
-      await pool.swap(SIDE_R, side, numberToWei(amount));
-      const balance = await derivable1155.balanceOf(
-        accountA.address,
-        packId(SIDE_A, pool.contract.address)
-      );
+INTEREST_HLS.forEach(INTEREST_HL => {
+  PREMIUM_HLS.forEach(PREMIUM_HL => {
+    describe("High Interest and Premium", function () {
+      describe(`I = ${INTEREST_HL}, P = ${PREMIUM_HL}`, function () {
+        const fixture = loadFixtureFromParams([
+          {
+            ...baseParams,
+            halfLife: bn(INTEREST_HL),
+            premiumHL: bn(PREMIUM_HL),
+          },
+        ]);
 
-      await time.increase(period);
+        async function openAndClosePosition(side, amount, period) {
+          const { derivablePools, derivable1155, accountA } = await loadFixture(
+            fixture
+          );
+          const pool = derivablePools[0].connect(accountA);
+          await pool.swap(SIDE_R, side, numberToWei(amount));
+          const balance = await derivable1155.balanceOf(
+            accountA.address,
+            packId(SIDE_A, pool.contract.address)
+          );
 
-      await pool.swap(side, SIDE_R, balance);
-    }
+          await time.increase(period);
 
-    it("Open long and close: 1e - 1 day", async function () {
-      await openAndClosePosition(SIDE_A, 1, 86400);
+          await pool.swap(side, SIDE_R, balance);
+        }
+
+        it("Open long and close: 1e - 1 day", async function () {
+          await openAndClosePosition(SIDE_A, 1, 86400);
+        });
+
+        it("Open short and close: 1e - 1 day", async function () {
+          await openAndClosePosition(SIDE_B, 1, 86400);
+        });
+
+        it("Open LP and close: 1e - 1 day", async function () {
+          await openAndClosePosition(SIDE_C, 1, 86400);
+        });
+      });
     });
-
-    it("Open short and close: 1e - 1 day", async function () {
-      await openAndClosePosition(SIDE_B, 1, 86400);
-    });
-
-    it("Open LP and close: 1e - 1 day", async function () {
-      await openAndClosePosition(SIDE_C, 1, 86400);
-    });
-  });
-});
+  })
+})
