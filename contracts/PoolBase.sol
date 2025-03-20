5da6577191b44b97405ac5f1ef96d71d2178ad5b
Zergity <zergity@gmail.com>
2025-03-20 12:22:23 +0700

    clone the ReentrancyGuardTransient contract from oz

--- contracts/PoolBase.sol
+++ contracts/PoolBase.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts/utils/ReentrancyGuardTransient.sol";
+import "./libs/ReentrancyGuardTransient.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 
