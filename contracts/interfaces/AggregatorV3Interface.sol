7a62a893764cad7a04435693dc8d4de19e3daf83
Zergity <zergity@gmail.com>
2025-06-26 10:40:47 +0700

    simple Chainlink fetcher

--- contracts/interfaces/AggregatorV3Interface.sol
+++ contracts/interfaces/AggregatorV3Interface.sol
@@ -10,14 +10,25 @@ interface AggregatorV3Interface {
 
     function latestRound() external view returns (uint256);
 
-    function getRoundData(
-        uint80 _roundId
-    )
+    function getRoundData(uint80 _roundId)
         external
         view
-        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+
     function latestRoundData()
         external
         view
-        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
 }
--- contracts/support/ChainlinkFetcher.sol
+++ contracts/support/ChainlinkFetcher.sol
@@ -6,25 +6,19 @@ import "../interfaces/AggregatorV3Interface.sol";
 import "../subs/Constants.sol";
 
 contract ChainlinkFetcher is Constants {
-    // DEVIATION(32bit)|DECIMALS(32bit) ... FEED(160bit)
+    // (32bit)|(32bit)|DECIMALS(32bit)|FEED(160bit)
     function fetch(
         uint256 ORACLE
     ) public view returns (uint256 twap, uint256 spot) {
-        int256 answer = _fetchPrice(address(uint160(ORACLE)));
-
-        uint32 decimals = uint32(ORACLE >> 192);
-        uint256 priceX128 = FullMath.mulDiv(uint256(answer), Q128, 10 ** decimals);
-
-        uint32 deviation = uint32(ORACLE >> 224);
-        if (deviation == 0) {
-            return (priceX128, priceX128);
-        }
-        twap = FullMath.mulDiv(priceX128, Q32 - deviation, Q32);
-        spot = FullMath.mulDivRoundingUp(priceX128, Q32 + deviation, Q32);
+        address feed = address(uint160(ORACLE));
+        uint256 price = _fetchChainlink(feed);
+        uint32 decimals = uint32(ORACLE >> 160);
+        twap = spot = FullMath.mulDiv(price, Q128, 10 ** decimals);
     }
 
-    function _fetchPrice(address feed) internal view returns (int256 answer) {
+    function _fetchChainlink(address feed) internal view returns (uint256 price) {
         AggregatorV3Interface aggregator = AggregatorV3Interface(feed);
-        (, answer, , , ) = aggregator.latestRoundData();
+        (, int256 answer, , , ) = aggregator.latestRoundData();
+        return uint256(answer);
     }
 }
